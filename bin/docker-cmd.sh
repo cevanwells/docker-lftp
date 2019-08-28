840529ec8ec02c6a365d999e4e924d38c08ae39c
Chris Wells <chris@cevanwells.com>
2019-08-28 09:45:05 -0400

    Replaced LOCAL_TARGET with data directory
    This was done because this image should be used as a one shot "--rm"
    and therefore should have its payload specified "-v" at each run.

--- bin/docker-cmd.sh
+++ bin/docker-cmd.sh
@@ -5,12 +5,11 @@ ARGS="--rcfile $APP_DIR/config/lftp.conf"
 
 case "$FTP_OP" in
 	"get"|"mget")
-		lftp $ARGS -c "mget -e -P 5 -O ${LOCAL_TARGET}/ ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET}"
+		lftp $ARGS -c "mget -e -P 5 -O data/ ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET}"
 		;;
 	"put"|"mput")
-		lftp $ARGS -c "mput -e -P 5 -O ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET} ${LOCAL_TARGET}"
+		lftp $ARGS -c "mput -e -P 5 -O ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET} data/"
 		;;
 	*)
 		echo "Operation not supported"
 esac
-# lftp --rcfile $APP_DIR/config/lftp.conf -c "${FTP_OP} -e -O ${LOCAL_TARGET}/ ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET}"
