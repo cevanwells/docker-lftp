573bdf58073c23defd9353a7aed893c243c97db4
Chris Wells <chris@cevanwells.com>
2019-08-28 15:46:22 -0400

    Re-added $LOCAL_TARGET and made it semi-mandatory

--- bin/docker-cmd.sh
+++ bin/docker-cmd.sh
@@ -5,10 +5,10 @@ ARGS="--rcfile $APP_DIR/config/lftp.conf"
 
 case "$FTP_OP" in
 	"get"|"mget")
-		lftp $ARGS -c "mget -e -P 5 -O data/ ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET}"
+		lftp $ARGS -c "mget -e -P 5 -O ${LOCAL_TARGET} ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET}"
 		;;
 	"put"|"mput")
-		lftp $ARGS -c "mput -e -P 5 -O ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET} data/"
+		lftp $ARGS -c "mput -e -P 5 -O ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET} ${LOCAL_TARGET}"
 		;;
 	*)
 		echo "Operation not supported"
--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 export FTP_OP=$1
 export FTP_TARGET=$2
-export LOCAL_TARGET=$3
+export LOCAL_TARGET=$APP_DIR/data
+if [ ! -z "$3" ]; then
+	export LOCAL_TARGET="${LOCAL_TARGET}/${3}"
+fi
 
 /usr/local/bin/docker-cmd.sh
