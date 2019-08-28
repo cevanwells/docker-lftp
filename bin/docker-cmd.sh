95a112ac78542ce0428577e4fdcb29f2ebe5be97
Chris Wells <chris@cevanwells.com>
2019-08-28 09:39:30 -0400

    Created case statement for command processing

--- bin/docker-cmd.sh
+++ bin/docker-cmd.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 set -e
 
+ARGS="--rcfile $APP_DIR/config/lftp.conf"
+
+case "$FTP_OP" in
+	"get"|"mget")
+		lftp $ARGS -c "mget -e -P 5 -O ${LOCAL_TARGET}/ ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET}"
+		;;
+	"put"|"mput")
+		lftp $ARGS -c "mput -e -P 5 -O ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET} ${LOCAL_TARGET}"
+		;;
+	*)
+		echo "Operation not supported"
+esac
+# lftp --rcfile $APP_DIR/config/lftp.conf -c "${FTP_OP} -e -O ${LOCAL_TARGET}/ ftps://${FTP_USER}:${FTP_PASS}@${FTP_IP}/${FTP_TARGET}"
